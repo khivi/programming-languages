463d62f14d528f5dc703d3fca2861d29d5cedc94
khivi <oss@khivi.com>
2013-07-18 00:05:55 -0400

    Use Scala Streams.

--- scala/src/main/scala/merge.scala
+++ scala/src/main/scala/merge.scala
@@ -1,33 +1,30 @@
 package com.khivi.merge
 
 object MergeSorted {
-  def apply[T <% Ordered[T]](collections: Seq[Iterable[T]]) =  new MergeSorted(collections)
+  def apply[T <% Ordered[T]](streams: Seq[Stream[T]]): Stream[T] =  new MergeSorted(streams).toStream
 }
 
-class MergeSorted[T <% Ordered[T]](collections: Seq[Iterable[T]]) extends Iterable[T] {
-    override def iterator: Iterator[T] = new MyIterator()
+class MergeSorted[T <% Ordered[T]](streams: Seq[Stream[T]]) {
 
-	private class MyIterator extends Iterator[T] {
-        private case class IterInfo(private val iter: Iterator[T]) {
-          def elem = curr
-          private[this] var curr = next
-          def nextElem { curr = next }
+      private type SI = (Stream[T], Int)
 
-          private[this] def next: Option[T] = elem match {
-            case None => None
-            case _ if (iter.hasNext) => Some(iter.next)
-            case _ => None
-          }
+      private[this] def getStream(streams: Seq[SI]): Stream[T] = {
+
+        def  getMin: Option[SI] = streams.foldRight(None: Option[SI]){ (stream, min)  =>
+          if (stream._1.isEmpty)
+            min
+          else if (min.isEmpty || stream._1.head < min.get._1.head)
+            Some(stream)
+          else
+            min
         }
 
-        private[this] val iters = collections.map{c => IterInfo(c.iterator) }
-
-        override def hasNext: Boolean = iters.exists(_.elem.isDefined)
-        override def next: T = {
-          val minIter = iters.filter(_.elem.isDefined).minBy(_.elem.get)
-          val min = minIter.elem
-          minIter.nextElem
-          min.get
+        getMin  match {
+          case None  => Stream.empty
+          case Some(min) =>  val newStreams: Seq[SI] = streams.updated(min._2, (min._1.tail, min._2))
+                                 min._1.head #:: getStream(newStreams)
         }
-	}
+      }
+
+    private def toStream: Stream[T] = getStream(streams.zipWithIndex)
 }
--- scala/src/test/scala/test.scala
+++ scala/src/test/scala/test.scala
@@ -2,36 +2,48 @@ import com.khivi.merge.MergeSorted
 
 
 import org.scalatest.FunSuite
+import scala.util.matching.Regex
 
 class TestSuite extends FunSuite {
   test("Unit test") {
     val collections = Seq(Seq("a", "d"),
                       Seq("a", "e", "g"),
-                      Seq("d", "g", "z"))
-    assert(MergeSorted(collections).toSeq === Seq("a", "a", "d", "d", "e", "g", "g", "z"))
+                      Seq("d", "g", "z")).map(_.toStream)
+    assert(MergeSorted(collections) === Stream("a", "a", "d", "d", "e", "g", "g", "z"))
   }
 
   test("File test" ) {
     import scala.io.Source
     val filename = "../data/test.txt"
 
-    class FileIterator(linePrefix: String) extends Iterable[Int] {
-      import scala.util.matching.Regex
+    class FileIterator(linePrefix: String) {
       private[this] val regex = new Regex(linePrefix + ":(.*)")
-      private[this] val elems = Source.fromFile(filename).getLines.flatMap{line => 
-        regex.findPrefixOf(line) match {
-          case Some(regex(x)) => x.split(",").map(_.toInt)
-          case None => Seq()
+      private[this] def getStream(lines: Stream[String], data: Stream[Int]): Stream[Int] = {
+        data.isEmpty match {
+          case true => lines.headOption match {
+                          case Some(line) =>  regex.findPrefixOf(line) match {
+                                                case Some(regex(x)) => val data = x.split(",").map(_.toInt).toStream 
+                                                                        data.isEmpty match {
+                                                                          case true => getStream(lines.tail, Stream.empty)
+                                                                          case false => data.head #:: getStream(lines.tail, data.tail)
+                                                                        }
+                                                case None => getStream(lines.tail, Stream.empty)
+                                              }
+                          case None => Stream.empty
+                        }
+          case false => data.head #:: getStream(lines, data.tail)
         }
       }
-      override def iterator = elems
+      def toStream: Stream[Int] = getStream(Source.fromFile(filename).getLines.toStream, Stream.empty)
+    }
+
+    object FileIterator {
+      def apply(linePrefix: String): Stream[Int] = new FileIterator(linePrefix).toStream
     }
 
     val number = Source.fromFile(filename).getLines.next.replaceAll("NUMBER=","").toInt
-    val collections = (0 until number).map{n =>
-                          new FileIterator("COLLECTION"+n)
-                      }
-    val needResult = new FileIterator("OUTPUT")
-    assert(MergeSorted(collections).toSeq === needResult.toSeq)
+    val collections = (0 until number).map(n=>FileIterator("COLLECTION"+n))
+    val needResult = FileIterator("OUTPUT")
+    assert(MergeSorted(collections) === needResult)
   }
 }
