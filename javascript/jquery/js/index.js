8c59d308eeb55a0163d8e7bd1782b0ef5b808289
khivi <oss@khivi.com>
2016-10-03 19:10:47 -0400

    Keep all state in the DOM

--- javascript/jquery/js/index.js
+++ javascript/jquery/js/index.js
@@ -1,9 +1,10 @@
 $(function() { 
-   var checkButton = function(data) {
-      var enabled = false;
-      $.each(data, function () {
-        if (this.length != 0) {
-          enabled = true;
+
+   var checkButton = function() {
+     var enabled = false;
+     $("#inputs .row").each(function() {
+       if ($(this).find('.elem').length !== 0) {
+         enabled = true;
        }
      });
      if (!enabled) {
@@ -11,56 +12,62 @@ $(function() {
      }
    };
 
-   var draw = function(data, output) {
-      $("#inputs .row:visible").remove();
-      $("#output .elem:visible").remove();
 
-      $.each(data, function () {
-        var row = $("<div>").addClass("row");
-        $.each(this, function() {
-          var elem = $("<div>").text(this.toString()).addClass("elem").addClass("col-md-1");
-          row.append(elem);
-        });
-        $("#inputs").append(row);
+   var mergeData = function () {
+     var minIdx = undefined;
+     var minValue = undefined;
+     $("#inputs .row").each(function() {
+       var idx = $(this).attr('idx');
+       var first = $(this).find('.elem:first');
+       if (first.length !== 0) { 
+         var currentValue = parseInt(first.text(), 10);
+         if (minValue === undefined || currentValue < minValue) { 
+           minIdx = idx;
+           minValue = currentValue;
+         }
+       }
      });
+     return minIdx;
+   };
 
-     $.each(output, function () {
-       var elem = $("<div>").text(this.toString()).addClass("elem").addClass("col-md-1");
+   var appendOutput = function(value) { 
+       var elem = $("<div>").text(value.toString()).addClass("col-md-1");
        $("#output").append(elem);
-     });
-
-     checkButton(data);
    };
 
-   var mergeData = function (data) {
-      var minIdx = undefined;
-      $.each(data, function (idx) {
-        if (minIdx === undefined  && this.length != 0) {
-          minIdx = idx;
-        }
-        else if (minIdx !== undefined && this.length > 0 && data[minIdx][0] > this[0]) {
-          minIdx = idx;
-        }
-      });
-      return minIdx;
-
+   var popInput = function(idx) { 
+       var elem = $("#inputs .row[idx="+idx+"] .elem:first");
+       elem.remove();
+       return elem.text();
    };
 
-  var data = [];
-  data[0] = [1, 2, 4, 7];
-  data[1] = [2, 4, 6];
-  var output = [];
+
 
   $(document).ready(function() {
-     draw(data, output);
+    var data = [];
+    data[0] = [1, 2, 4, 7];
+    data[1] = [2, 4, 6];
+    data[2] = [2, 4, 8];
+    var initialDraw = function() {
+       $.each(data, function (idx) {
+         var row = $("<div>").addClass("row").attr('idx', idx);
+         $.each(this, function() {
+           var elem = $("<div>").text(this.toString()).addClass('elem').addClass("col-md-1");
+           row.append(elem);
+         });
+         $("#inputs").append(row);
+      });
+    };
+    initialDraw();
+    checkButton();
   });
 
   $("#nextButton").click(function() {
-      var idx = mergeData(data);
+      var idx = mergeData();
       if (idx !== undefined) { 
-        var value = data[idx].shift();
-        output.push(value);
+        var value = popInput(idx);
+        appendOutput(value);
+        checkButton();
       }
-      draw(data, output);
   });
 });
