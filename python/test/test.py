20614015917ba9764e47e124bf9066b6412ac9e3
khivi <Amit.Khivesara@target.com>
2017-03-21 17:00:02 -0400

    Added tests for pythong for infinite lists

--- python/test/test.py
+++ python/test/test.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 
 from merge import Merge
 
@@ -9,4 +10,29 @@ class TestMerge(unittest.TestCase):
         merge = Merge(lists)
         generator = merge.generator()
         assert(list(generator) == [1,2,2,3,3,4,5])
+
+    def test_strings(self):
+        lists = (("b","e"), ("c","d"), ("a","z"))
+        lists = [iter(l) for l in lists]
+        merge = Merge(lists)
+        generator = merge.generator()
+        assert(list(generator) == ['a', 'b', 'c','d','e','z'])
+
+    def test_infinity(self):
+        def infinity(value):
+            while True:
+                yield value
+
+        merge = Merge((infinity(1), infinity(2)))
+        generator = merge.generator()
+        assert(next(generator) == 1)
+
+    def test_range(self):
+        cnt = 5
+        lists = [ iter(xrange(sys.maxint)) for _ in range(cnt) ]
+        merge = Merge(lists)
+        generator = merge.generator()
+        for value in range(10):
+            for _ in range(cnt):
+                assert(next(generator) == value)
 
