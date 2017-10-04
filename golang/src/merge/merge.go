c4b7cd2c505123ef566c8d77d3951498961a05a6
khivi <Amit.Khivesara@target.com>
2017-10-04 12:28:21 -0400

    Parse lines

--- golang/src/merge/merge.go
+++ golang/src/merge/merge.go
@@ -3,6 +3,8 @@ package merge
 import "bufio"
 import "os"
 import "regexp"
+import "strconv"
+import "strings"
 
 
 func f1(v int) int {
@@ -10,13 +12,30 @@ func f1(v int) int {
 }
 
 
-func readFile(fileName string, pattern string) (lines []string) {
+func readFileWithPrefixMatch(fileName string, pattern string) (lines []string) {
     file, _ := os.Open(fileName)
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         var line = scanner.Text()
-        if matched, _ := regexp.MatchString(pattern, line); matched {
+        if matched, _ := regexp.MatchString("^"+pattern, line); matched {
+            line = strings.TrimPrefix(line, pattern)
             lines = append(lines, line)
+        }
+    }
+    return
+}
+
+func readNumber(fileName string, pattern string) int {
+    lines := readFileWithPrefixMatch(fileName, pattern)
+    number, _ := strconv.Atoi(lines[0])
+    return number
+}
+
+func readNumberList(fileName string, pattern string) (numbers []int) {
+    for _, line := range readFileWithPrefixMatch(fileName, pattern) {
+        for _, s := range strings.Split(line, ",") {
+            number, _ := strconv.Atoi(s)
+            numbers = append(numbers, number)
         }
     }
     return
--- golang/src/merge/merge_test.go
+++ golang/src/merge/merge_test.go
@@ -1,6 +1,7 @@
 package merge
 
 import "testing"
+import "reflect"
 
 func TestF1(t *testing.T) {
     if (f1(1) != 2)  {
@@ -8,13 +9,30 @@ func TestF1(t *testing.T) {
     }
 }
 
+var fileName = "../../../data/test.txt"
+
 func TestReadFile(t *testing.T) {
-    lines := readFile("../../../data/test.txt", "^NUMBER")
+    lines := readFileWithPrefixMatch(fileName, "NUMBER=")
     if (len(lines) != 1)  {
         t.Error(`len(lines) failed`)
     }
-    if (lines[0] != "NUMBER=3") {
+    if (lines[0] != "3") {
         t.Error(`lines[0] failed`)
+    }
+}
+
+func TestReadNumber(t *testing.T) {
+    number := readNumber(fileName, "NUMBER=")
+    if (number != 3) {
+        t.Error(`TestReadNumber failed`)
+    }
+}
+
+func TestReadNumberList(t *testing.T) {
+    numbers := readNumberList(fileName, "OUTPUT:")
+    expected := []int {1,1,2,2,3,4,4,6,7,9,9,20,21}
+    if (!reflect.DeepEqual(numbers, expected)) {
+        t.Error(`TestReadNumberList failed`)
     }
 }
 
