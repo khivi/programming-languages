4f24c90766d79247ee2e8e005b38179381fe98f0
khivi <Amit.Khivesara@target.com>
2017-10-04 15:15:14 -0400

    go fmt

--- golang/src/merge/merge.go
+++ golang/src/merge/merge.go
@@ -7,95 +7,92 @@ import "regexp"
 import "strconv"
 import "strings"
 
-
 func f1(v int) int {
-    return v+1
+	return v + 1
 }
 
-
 func readFileWithPrefixMatch(fileName string, pattern string, out chan<- string) {
-    file, _ := os.Open(fileName)
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        var line = scanner.Text()
-        if matched, _ := regexp.MatchString("^"+pattern, line); matched {
-            line = strings.TrimPrefix(line, pattern)
-            out <- line
-        }
-    }
-    close(out)
-    return
+	file, _ := os.Open(fileName)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var line = scanner.Text()
+		if matched, _ := regexp.MatchString("^"+pattern, line); matched {
+			line = strings.TrimPrefix(line, pattern)
+			out <- line
+		}
+	}
+	close(out)
+	return
 }
 
 func readNumber(fileName string, pattern string) int {
-    ch := make(chan string)
-    go readFileWithPrefixMatch(fileName, pattern, ch)
-    line := <-ch
-    number, _ := strconv.Atoi(line)
-    return number
+	ch := make(chan string)
+	go readFileWithPrefixMatch(fileName, pattern, ch)
+	line := <-ch
+	number, _ := strconv.Atoi(line)
+	return number
 }
 
 func readNumberList(fileName string, pattern string, out chan<- int) {
-    ch := make(chan string)
-    go readFileWithPrefixMatch(fileName, pattern, ch)
-    for line:= range ch {
-        for _, s := range strings.Split(line, ",") {
-            number, _ := strconv.Atoi(s)
-            out <- number
-        }
-    }
-    close(out)
-    return
+	ch := make(chan string)
+	go readFileWithPrefixMatch(fileName, pattern, ch)
+	for line := range ch {
+		for _, s := range strings.Split(line, ",") {
+			number, _ := strconv.Atoi(s)
+			out <- number
+		}
+	}
+	close(out)
+	return
 }
 
 func getNumber(fileName string) int {
-    return readNumber(fileName, "NUMBER=")
+	return readNumber(fileName, "NUMBER=")
 }
 
-func getOutput(fileName string, out chan<- int ) {
-    readNumberList(fileName, "OUTPUT:", out)
-    return
+func getOutput(fileName string, out chan<- int) {
+	readNumberList(fileName, "OUTPUT:", out)
+	return
 }
 
-func findMin(numbers[]* int) (minIdx int) {
-    minIdx = -1
-    for idx, number := range numbers {
-        if number != nil {
-            if minIdx == -1 || *numbers[minIdx] > *numbers[idx] {
-                minIdx = idx
-            }
-        }
-    }
-    return
+func findMin(numbers []*int) (minIdx int) {
+	minIdx = -1
+	for idx, number := range numbers {
+		if number != nil {
+			if minIdx == -1 || *numbers[minIdx] > *numbers[idx] {
+				minIdx = idx
+			}
+		}
+	}
+	return
 }
 
 func Merge(fileName string, out chan<- int) {
-    number := getNumber(fileName)
-    channels := make([]chan int, number)
-    for i:= 0; i < number;  i++ {
-        pattern := fmt.Sprintf("COLLECTION%d:",i)
-        ch := make(chan int)
-        go readNumberList(fileName, pattern, ch)
-        channels[i] = ch
-    }
-    values := make([]* int, number)
-    for {
-        for i, value := range values {
-            if value == nil {
-                ch := channels[i]
-                if number, ok := <-ch; ok  {
-                    values[i] = &number
-                }
-            }
-        }
-        minIdx := findMin(values)
-        if minIdx == -1 {
-            break
-        }
-        out <- *values[minIdx]
-        values[minIdx] = nil
-    }
-    close(out)
-    return
+	number := getNumber(fileName)
+	channels := make([]chan int, number)
+	for i := 0; i < number; i++ {
+		pattern := fmt.Sprintf("COLLECTION%d:", i)
+		ch := make(chan int)
+		go readNumberList(fileName, pattern, ch)
+		channels[i] = ch
+	}
+	values := make([]*int, number)
+	for {
+		for i, value := range values {
+			if value == nil {
+				ch := channels[i]
+				if number, ok := <-ch; ok {
+					values[i] = &number
+				}
+			}
+		}
+		minIdx := findMin(values)
+		if minIdx == -1 {
+			break
+		}
+		out <- *values[minIdx]
+		values[minIdx] = nil
+	}
+	close(out)
+	return
 }
-
--- golang/src/merge/merge_test.go
+++ golang/src/merge/merge_test.go
@@ -4,76 +4,76 @@ import "reflect"
 import "testing"
 
 func TestF1(t *testing.T) {
-    if f1(1) != 2 {
-        t.Error(`f1 failed`)
-    }
+	if f1(1) != 2 {
+		t.Error(`f1 failed`)
+	}
 }
 
 var fileName = "../../../data/test.txt"
 
 func TestReadFile(t *testing.T) {
-    ch := make(chan string)
-    go readFileWithPrefixMatch(fileName, "NUMBER=", ch)
-    line := <- ch
-    if line != "3" {
-        t.Error(`lines[0] failed`)
-    }
+	ch := make(chan string)
+	go readFileWithPrefixMatch(fileName, "NUMBER=", ch)
+	line := <-ch
+	if line != "3" {
+		t.Error(`lines[0] failed`)
+	}
 }
 
 func TestReadNumber(t *testing.T) {
-    number := readNumber(fileName, "NUMBER=")
-    if number != 3 {
-        t.Error(`TestReadNumber failed`)
-    }
+	number := readNumber(fileName, "NUMBER=")
+	if number != 3 {
+		t.Error(`TestReadNumber failed`)
+	}
 }
 
 func TestReadNumberList(t *testing.T) {
-    ch := make(chan int)
-    go readNumberList(fileName, "OUTPUT:", ch)
-    actual := chanToSlice(ch)
-    expected := []int {1,1,2,2,3,4,4,6,7,9,9,20,21}
-    if !reflect.DeepEqual(actual, expected) {
-        t.Error(`TestReadNumberList failed`)
-    }
+	ch := make(chan int)
+	go readNumberList(fileName, "OUTPUT:", ch)
+	actual := chanToSlice(ch)
+	expected := []int{1, 1, 2, 2, 3, 4, 4, 6, 7, 9, 9, 20, 21}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Error(`TestReadNumberList failed`)
+	}
 }
 
 func chanToSlice(ch <-chan int) (numbers []int) {
-    for number := range ch {
-        numbers = append(numbers, number)
-    }
-    return
+	for number := range ch {
+		numbers = append(numbers, number)
+	}
+	return
 }
 
 func compareChannels(ch1 <-chan int, ch2 <-chan int) bool {
-    for {
-        e1, ok1 := <-ch1
-        e2, ok2 := <-ch2
-        if !ok1 && !ok2 {
-            return true
-        }
-        if e1 != e2 {
-            return false
-        }
-    }
+	for {
+		e1, ok1 := <-ch1
+		e2, ok2 := <-ch2
+		if !ok1 && !ok2 {
+			return true
+		}
+		if e1 != e2 {
+			return false
+		}
+	}
 }
 
 func TestMerge(t *testing.T) {
-    actual := make(chan int)
-    expected := make(chan int)
-    go Merge(fileName, actual)
-    go getOutput(fileName, expected)
-    if !compareChannels(actual, expected) {
-        t.Error(`TestMerge failed`)
-    }
+	actual := make(chan int)
+	expected := make(chan int)
+	go Merge(fileName, actual)
+	go getOutput(fileName, expected)
+	if !compareChannels(actual, expected) {
+		t.Error(`TestMerge failed`)
+	}
 }
 
 func TestBadMerge(t *testing.T) {
-    actual := make(chan int)
-    expected := make(chan int)
-    fileName = "../../../data/err.txt"
-    go Merge(fileName, actual)
-    go getOutput(fileName, expected)
-    if compareChannels(actual, expected) {
-        t.Error(`TestBadMerge failed`)
-    }
+	actual := make(chan int)
+	expected := make(chan int)
+	fileName = "../../../data/err.txt"
+	go Merge(fileName, actual)
+	go getOutput(fileName, expected)
+	if compareChannels(actual, expected) {
+		t.Error(`TestBadMerge failed`)
+	}
 }
