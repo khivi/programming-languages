b402edc39969e852bdbb88c5ab90d94dd1996f7e
khivi <work@khivi.com>
2020-05-04 17:30:36 -0400

    Using ava test framekwork

--- nodejs/test/test.js
+++ nodejs/test/test.js
@@ -1,10 +1,8 @@
-var assert = require('chai').assert
+const test = require('ava');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+
+test('should return -1 when the value is not present', t => {
+    t.assert([1, 2, 3].indexOf(4) == -1);
+})
+
 
