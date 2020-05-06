c70198eaaa7a8986d631ebd2b9478d23f6f429ca
khivi <work@khivi.com>
2020-05-06 14:46:42 -0400

    Moved paths to helper

--- nodejs/test/file_data.js
+++ nodejs/test/file_data.js
@@ -2,20 +2,16 @@ const test = require('ava');
 const file = require('../src/file');
 const DATA = require('./helpers/data');
 
-test('read number from test', (t) => {
+
+test('read output from test', (t) => {
   return file.getNumber(DATA.TEST).then((number) => {
     t.is(number, 3);
   });
 });
 
-test('read number from err', (t) => {
+test('read output from err', (t) => {
   return file.getNumber(DATA.ERR).then((number) => {
     t.is(number, 1);
   });
 });
 
-test('throw error from bad', async (t) => {
-  const promise = file.getNumber(DATA.BAD);
-  const e = await t.throwsAsync(promise);
-  t.is(e.message, 'Number not found');
-});
---
+++ nodejs/test/helpers/data.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const _DATA_DIR = path.resolve(__dirname, '..', '..', '..', 'data');
+const TEST= path.resolve(_DATA_DIR, 'test.txt');
+const ERR= path.resolve(_DATA_DIR, 'err.txt');
+const BAD= path.resolve(_DATA_DIR, 'bad.txt');
+
+exports.TEST=TEST
+exports.ERR=ERR
+exports.BAD=BAD
