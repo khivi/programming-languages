e3b250fe117178bcbc827af1450f1d8dc7ee99cd
khivi <work@khivi.com>
2020-05-07 13:06:55 -0400

    Test for err collection

--- nodejs/test/file_collection.js
+++ nodejs/test/file_collection.js
@@ -15,17 +15,26 @@ function collection(filename, num) {
   return _actual(data);
 }
 
-test('read output from test', async (t) => {
+test('read collection', async (t) => {
   t.plan(1);
   const expected = [2, 4, 6, 9, 20];
   const actual = await collection(DATA.TEST, 1);
   t.deepEqual(actual, expected);
 });
 
-test('read output from bad', async (t) => {
+
+test('read collection not found', async (t) => {
   t.plan(1);
   const expected = [];
-  const actual = await collection(DATA.BAD, 0);
+  const actual = await collection(DATA.TEST, 5);
   t.deepEqual(actual, expected);
+});
+
+test('read output from err', async (t) => {
+  t.plan(2);
+  const data = file.getData(DATA.ERR, 'COLLECTION0');
+  const next = async () => (await data.next()).value;
+  t.deepEqual(1, await next());
+  t.deepEqual(1, await next());
 });
 
