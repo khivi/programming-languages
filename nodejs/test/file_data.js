05b02f41d5b857a922bacaa839ca1241c3e5f115
khivi <work@khivi.com>
2020-05-07 11:25:30 -0400

    lint

--- nodejs/test/file_data.js
+++ nodejs/test/file_data.js
@@ -4,16 +4,19 @@ const DATA = require('./helpers/data');
 
 
 test('read output from test', async (t) => {
+  const expected = [1, 1, 2, 2, 3, 4, 4, 6, 7, 9, 9, 20, 21];
   t.plan(1);
   const data = file.getData(DATA.TEST, 'OUTPUT');
+  let i = 0;
   for await (const n of data) {
-    t.deepEqual(n, 2);
+    t.is(expected[i], n);
+    i++;
   }
 });
 
-test('read output from err', async (t) => {
+test('read output from err', (t) => {
   t.plan(1);
-  const data = await file.getData(DATA.ERR, 'OUTPUT');
+  const data = file.getData(DATA.ERR, 'OUTPUT');
   t.deepEqual([...data], [3]);
 });
 
--- nodejs/test/file_number.js
+++ nodejs/test/file_number.js
@@ -2,18 +2,16 @@ const test = require('ava');
 const file = require('../src/file');
 const DATA = require('./helpers/data');
 
-test('read number from test', (t) => {
+test('read number from test', async (t) => {
   t.plan(1);
-  return file.getNumber(DATA.TEST).then((number) => {
-    t.is(number, 3);
-  });
+  const number = await file.getNumber(DATA.TEST);
+  t.is(number, 3);
 });
 
-test('read number from err', (t) => {
+test('read number from err', async (t) => {
   t.plan(1);
-  return file.getNumber(DATA.ERR).then((number) => {
-    t.is(number, 1);
-  });
+  const number = await file.getNumber(DATA.ERR);
+  t.is(number, 1);
 });
 
 test('throw error from bad', async (t) => {
