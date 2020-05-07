d079a0afabf0f928cba4fd9bc375fd3872590c1e
khivi <work@khivi.com>
2020-05-07 12:25:56 -0400

    Working OUTPUT

--- nodejs/test/file_data.js
+++ nodejs/test/file_data.js
@@ -3,20 +3,35 @@ const file = require('../src/file');
 const DATA = require('./helpers/data');
 
 
-test('read output from test', async (t) => {
-  const expected = [1, 1, 2, 2, 3, 4, 4, 6, 7, 9, 9, 20, 21];
-  t.plan(expected.length);
-  const data = file.getData(DATA.TEST, 'OUTPUT');
-  let i = 0;
+async function _actual(data) {
+  let actual = []
   for await (const n of data) {
-    t.is(expected[i], n, `index:${i}, value:${n}`);
-    i++;
+    actual.push(n);
   }
+  return actual;
+}
+
+test('read output from test', async (t) => {
+  t.plan(1);
+  const expected = [1, 1, 2, 2, 3, 4, 4, 6, 7, 9, 9, 20, 21];
+  const data = file.getData(DATA.TEST, 'OUTPUT');
+  let actual = await _actual(data);
+  t.deepEqual(actual, expected);
 });
 
-test('read output from err', (t) => {
+test('read output from err', async (t) => {
   t.plan(1);
+  const expected = [10, 200, 3000];
   const data = file.getData(DATA.ERR, 'OUTPUT');
-  t.deepEqual([...data], [3]);
+  let actual = await _actual(data);
+  t.deepEqual(actual, expected);
+});
+
+test('read output from bad', async (t) => {
+  t.plan(1);
+  const expected = [];
+  const data = file.getData(DATA.BAD, 'OUTPUT');
+  let actual = await _actual(data);
+  t.deepEqual(actual, expected);
 });
 
