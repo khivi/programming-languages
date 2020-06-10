cdcc6ede536379b01bfb8b26b2be90679e87849b
khivi <work@khivi.com>
2020-06-10 15:01:50 -0400

    cleanup test

--- react-js/src/__tests__/App.test.jsx
+++ react-js/src/__tests__/App.test.jsx
@@ -3,13 +3,9 @@ import { render } from '@testing-library/react';
 
 import App from '../App';
 
-test('renders hey ', () => {
+test('renders witout name ', () => {
   const { getByText } = render(<App />);
   const text = getByText(/hey/i);
   expect(text).toBeInTheDocument();
 });
 
-test('renders hey text ', () => {
-  const { container } = render(<App />);
-  expect(container.firstChild.firstChild).toHaveTextContent(/hey/i);
-});
