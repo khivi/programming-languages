ea5a527ca89dbb0060116352030396ad913b6e75
khivi <work@khivi.com>
2020-06-12 15:33:44 -0400

    remove jsconfig support

--- react-js/config/modules.js
+++ react-js/config/modules.js
@@ -99,31 +99,10 @@ function getJestAliases(options = {}) {
 }
 
 function getModules() {
-  // Check if TypeScript is setup
-  const hasTsConfig = fs.existsSync(paths.appTsConfig);
-  const hasJsConfig = fs.existsSync(paths.appJsConfig);
-
-  if (hasTsConfig && hasJsConfig) {
-    throw new Error(
-      'You have both a tsconfig.json and a jsconfig.json. If you are using TypeScript please remove your jsconfig.json file.'
-    );
-  }
-
-  let config;
-
-  // If there's a tsconfig.json we assume it's a
-  // TypeScript project and set up the config
-  // based on tsconfig.json
-  if (hasTsConfig) {
-    const ts = require(resolve.sync('typescript', {
-      basedir: paths.appNodeModules,
-    }));
-    config = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config;
-    // Otherwise we'll check if there is jsconfig.json
-    // for non TS projects.
-  } else if (hasJsConfig) {
-    config = require(paths.appJsConfig);
-  }
+  const ts = require(resolve.sync('typescript', {
+    basedir: paths.appNodeModules,
+  }));
+  let config = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config;
 
   config = config || {};
   const options = config.compilerOptions || {};
@@ -133,8 +112,7 @@ function getModules() {
   return {
     additionalModulePaths: additionalModulePaths,
     webpackAliases: getWebpackAliases(options),
-    jestAliases: getJestAliases(options),
-    hasTsConfig,
+    jestAliases: getJestAliases(options)
   };
 }
 
