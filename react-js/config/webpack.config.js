227e3aad725b69e7c9be1579ab9dbf05d9d86d3c
khivi <work@khivi.com>
2020-06-16 13:33:02 -0400

    remove react-native

--- react-js/config/webpack.config.js
+++ react-js/config/webpack.config.js
@@ -150,9 +150,6 @@ module.exports = function(webpackEnv) {
         .map(ext => `.${ext}`)
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
-        // Support React Native Web
-        // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-        'react-native': 'react-native-web',
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
