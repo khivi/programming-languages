4d479e514026e575fe733e1f61abdd3f4b58a2f9
khivi <work@khivi.com>
2021-03-08 14:30:47 -0500

    File and regex for number

---
+++ rust/src/lib.rs
@@ -0,0 +1,50 @@
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use regex::Regex;
+
+
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a+b
+}
+
+
+#[macro_use]
+extern crate lazy_static;
+pub fn get_number<P: AsRef<Path>>(filename: P) -> io::Result<i32> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"NUMBER=(\d+)").unwrap();
+    }
+    let lines = read_lines(filename)?;
+    for line in lines {
+        if let Some(n) = RE.captures(&line.unwrap()) {
+            if let Some(n) = n.get(1) {
+                return Ok(n.as_str().parse().unwrap());
+            }
+        }
+    }
+    Ok(0)
+}
+
+fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add(2,2) , 4);
+    }
+
+    #[test]
+    fn get_test_number() {
+        assert_eq!(get_number("../data/test.txt").unwrap() , 3);
+    }
+}
