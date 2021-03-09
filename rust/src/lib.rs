cf6d28d38203d4c8d3a8476f9eeff1fc5d968f7c
khivi <work@khivi.com>
2021-03-09 17:05:54 -0500

    Works. Regex lifetime not correct yet

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,19 +3,41 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use regex::Regex;
 
-#[macro_use]
-extern crate lazy_static;
+//#[macro_use]
+//extern crate lazy_static;
 pub fn get_number<P: AsRef<Path>>(filename: P) -> io::Result<u32> {
+    /*
     lazy_static! {
         static ref RE: Regex = Regex::new(r"NUMBER=(\d+)").unwrap();
     }
-    let mut matches = match_lines(filename, &RE)?;
+    */
+    //let RE: Regex = Regex::new(r"NUMBER=(\d+)").unwrap();
+    let regex: Regex = Regex::new(r"OUTPUT:(.*)").unwrap();
+    let mut matches = match_lines(filename, &regex)?;
     return Ok(matches.next().unwrap().parse().unwrap());
 }
 
-fn match_lines<P: AsRef<Path>>(filename: P, regex: &'static Regex) -> io::Result<impl Iterator<Item = String>> {
+
+
+pub fn get_output<P: AsRef<Path>>(filename: P) -> io::Result<impl Iterator<Item = i32>> {
+    let regex: Regex = Regex::new(r"OUTPUT:(.*)").unwrap();
+    let matches = match_lines(filename, &regex)?;
+    let values = matches.map(|s| 
+        s.split(',').map(|v| v.parse().unwrap()).collect::<Vec<_>>().into_iter()
+    ).flatten();
+    Ok(values)
+}
+
+fn captured(regex: &Regex, index: usize, line: &str) -> String {
+    regex.captures(&line).unwrap()[index].to_string()
+}
+
+fn match_lines<P: AsRef<Path>>(filename: P, _regex: &Regex) -> io::Result<impl Iterator<Item = String>> {
     let lines = read_lines(filename)?;
-    let matches = lines.filter_map(move |line| regex.captures(&line).map(|captures| captures[1].to_string()));
+    let matches = lines.filter_map(|line| 
+        //Some(captured(regex, &line))
+        Some(captured(&Regex::new(r"NUMBER=(.*)").unwrap(), 1, &line))
+    );
     Ok(matches)
 }
 
@@ -30,6 +52,13 @@ fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<impl Iterator<Item = St
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_captured() {
+        let regex: Regex = Regex::new(r"OUTPUT:(\d+),(.*)").unwrap();
+        assert_eq!(captured(&regex, 1, "OUTPUT:1231,"), "1231");
+        assert_eq!(captured(&regex, 2, "OUTPUT:3234,rust"), "rust");
+    }
 
     #[test]
     fn get_test_number() {
