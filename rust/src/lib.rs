13986f3b3dcfe44e716f665f33f24f3d9de337f4
khivi <work@khivi.com>
2021-03-10 12:38:15 -0500

    works

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,43 +1,42 @@
+
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use regex::Regex;
 
-//#[macro_use]
-//extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 pub fn get_number<P: AsRef<Path>>(filename: P) -> io::Result<u32> {
-    /*
     lazy_static! {
         static ref RE: Regex = Regex::new(r"NUMBER=(\d+)").unwrap();
     }
-    */
-    //let RE: Regex = Regex::new(r"NUMBER=(\d+)").unwrap();
-    let regex: Regex = Regex::new(r"OUTPUT:(.*)").unwrap();
-    let mut matches = match_lines(filename, &regex)?;
+    let mut matches = match_lines(filename, &*RE)?;
     return Ok(matches.next().unwrap().parse().unwrap());
 }
 
 
 
-pub fn get_output<P: AsRef<Path>>(filename: P) -> io::Result<impl Iterator<Item = i32>> {
-    let regex: Regex = Regex::new(r"OUTPUT:(.*)").unwrap();
-    let matches = match_lines(filename, &regex)?;
+pub fn get_output<'a, P:'a + AsRef<Path>>(filename: P) -> io::Result<impl Iterator<Item = i32> + 'a> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"OUTPUT:(.*)").unwrap();
+    }
+    let matches = match_lines(filename, &*RE)?;
     let values = matches.map(|s| 
         s.split(',').map(|v| v.parse().unwrap()).collect::<Vec<_>>().into_iter()
     ).flatten();
     Ok(values)
 }
 
-fn captured(regex: &Regex, index: usize, line: &str) -> String {
-    regex.captures(&line).unwrap()[index].to_string()
-}
 
-fn match_lines<P: AsRef<Path>>(filename: P, _regex: &Regex) -> io::Result<impl Iterator<Item = String>> {
+fn match_lines<'a, P: 'a + AsRef<Path>>(filename: P, regex: &'a Regex) -> io::Result<impl Iterator<Item = String> + 'a> {
     let lines = read_lines(filename)?;
-    let matches = lines.filter_map(|line| 
-        //Some(captured(regex, &line))
-        Some(captured(&Regex::new(r"NUMBER=(.*)").unwrap(), 1, &line))
-    );
+    let matches = lines.filter_map(move |line|  {
+        if let Some(captures) = regex.captures(&line) {
+            Some(captures[1].to_string())
+        } else {
+            None
+        }
+    });
     Ok(matches)
 }
 
@@ -54,14 +53,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_captured() {
-        let regex: Regex = Regex::new(r"OUTPUT:(\d+),(.*)").unwrap();
-        assert_eq!(captured(&regex, 1, "OUTPUT:1231,"), "1231");
-        assert_eq!(captured(&regex, 2, "OUTPUT:3234,rust"), "rust");
+    fn test_number() {
+        assert_eq!(get_number("../data/test.txt").unwrap() , 3);
     }
 
     #[test]
-    fn get_test_number() {
-        assert_eq!(get_number("../data/test.txt").unwrap() , 3);
+    fn test_output() {
+        let output = [1,1,2,2,3,4,4,6,7,9,9,20,21];
+        assert_eq!(get_output("../data/test.txt").unwrap().collect::<Vec<_>>(), output);
     }
 }
